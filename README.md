ca4fa2ba8969df359c4a537a8f5fd788b5091efb
kevdev-11 <wholesolution.klb@gmail.com>
2024-04-02 22:33:45 -0300

    cambiokevin

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 
 Primer readme hecho por Diego para empezar con el proyecto del dashborad 😀😀🤣😀
 
-<<<<<<< HEAD
 
 Nuevo cambio Diego
-=======
 Cambio 2 en el readme
->>>>>>> 7e034aa4f3e4f532a116beb83df830df4f0b44e7
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const handleChange = () => {
+    setCount(count => count + 1 )
+  }
 
   return (
     <>
@@ -18,7 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 3)}>
+        <button onClick={handleChange}>
           count is {count}
         </button>
         <p>
